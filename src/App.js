c2791b9945a9fde78f8feb0bf6e1225ffdc2d122
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-19 09:53:54 +0600

    add react-core-concept-recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent name="React Babu" price="50000"></MyComponent>
+      <MyComponent name ="Google Mama" price ="45600"></MyComponent>
+      <MyComponent name="Apple Chacha" price="45200"></MyComponent>
     </div>
   );
+}
+
+function MyComponent(props) {
+  const [point, setPoint] = useState(1)
+
+  const myStyle = {
+    padding: '10px',
+    borderRadius: '20px',
+    margin: ' 20px'
+  }
+
+  const handleAddPoint = () =>{
+    const newPoint = point * 2;
+    setPoint(newPoint);
+  }
+
+  return (
+    <div className="my-component" style={myStyle}>
+      <h1>Yo yo mama! I am {props.name}</h1>
+      <h3>How much Rate for you: {props.price} and I have points: {point}</h3>
+     
+      <button onClick = {handleAddPoint} >Add Points</button>
+      <p style={{ color: "blue", fontWeight: "bold" }}>ok you are right place, so you can do this every thing </p>
+    </div>
+  )
 }
 
 export default App;
